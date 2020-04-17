0b5cec618b65d210f5b860f6002de6b17e729f23
Hayley Ha <hami201289@him-mail.ch>
2020-04-17 13:14:53 +1200

    formatted backend index.js

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcryptjs = require('bcryptjs');
 const config = require('./config.json'); // store creditials
+
 const Comment = require('./models/comment.js');
 const Member = require('./models/member.js');
 const Portfolio = require('./models/portfolio.js');
 
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World from Hayley, Rahul and Yana!'));
-
+// CONNECT TO DATABASE WITH MONGOOSE
 const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}-tvmnw.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('DB connected'))
@@ -22,28 +22,28 @@ mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
   console.log(`DBConnectionError: ${err.message}`);
 });
 
-// test connection
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
   console.log('We are connected to MongoDB');
 });
 
-// connect endpoints
-app.use((req,res,next)=>{
+// APP SET-UPS
+app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`);
-  next();//include this to go to the next middleware
+  next();
 });
-
-// include body-parser, cors, bcryptjs
+app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
 app.use(cors());
 
-// register a member ============================================================
-// Yanas code
 
+// ===================================================================================
+// ============================= YANA's code starts ==================================
+
+
+// Register a member
 app.post('/registerMember', (req,res) => {
   //checking if member is found in the db already
   Member.findOne({username:req.body.username},(err,memberResult)=> {
@@ -69,18 +69,14 @@ app.post('/registerMember', (req,res) => {
   });
 });
 
-// get all members =============================================================
-// Yanas code
-
+// Get all members
 app.get('/allMembers', (req,res) => {
   Member.find().then(result => {
     res.send(result);
   });
 });
 
-// login a member ==============================================================
-// Yanas code
-
+// Login a member
 app.post('/loginMember', (req,res) => {
   Member.findOne({username:req.body.username},(err,memberResult) => {
     if (memberResult){
@@ -96,10 +92,7 @@ app.post('/loginMember', (req,res) => {
 });
 
 
-
-// add an artwork to portfolio =================================================
-// Yanas code
-
+// Add an artwork to portfolio
 app.post('/addPortfolio', (req,res) => {
   //checking if portfolio is found in the db already
   Portfolio.findOne({title:req.body.title},(err,portfolioResult)=>{
@@ -123,9 +116,7 @@ app.post('/addPortfolio', (req,res) => {
   });
 });
 
-// get all portfolios ==========================================================
-// Yanas code
-
+// Get all portfolios
 app.get('/allPortfolios', (req,res) => {
   Portfolio.find().then(result => {
     console.log(result);
@@ -133,65 +124,145 @@ app.get('/allPortfolios', (req,res) => {
   });
 });
 
+// Delete a project
+app.delete('/deletePortfolio/:id',(req,res)=>{
+  const idParam = req.params.id;
+  Portfolio.findOne({_id:idParam}, (err,portfolio)=>{
+    if (portfolio){
+      Portfolio.deleteOne({_id:idParam},err=>{
+        res.send('Portfolio deleted');
+      });
+    } else {
+      res.send('Portfolio not found');
+    }
+  }).catch(err => res.send(err)); //refers to mogodb id
+});
 
-//  Yanas code ends
-// =============================================================================
+
+// =========================== YANA's code ends  =====================================
+// ===================================================================================
 
 
-// Hayley's code
-app.get('/myAccountInfo/:accountID', (req, res) => {
-  let _memberId = req.params.accountID;
-  Member.findById(_memberId, function(err, result) {
-    res.send(result);
-  })
+// ===================================================================================
+// =========================== HAYLEY's code starts ==================================
+
+// Update a member info
+app.patch('/updateMember/:id', (req, res) => {
+  const _id = req.params.id;
+  const updatedMember = {
+                          username: req.body.username,
+                          email: req.body.email,
+                          about: req.body.about,
+                          location: req.body.location,
+                          website: req.body.website
+  }
+
+  Member.findByIdAndUpdate(_id, 
+                            { $set: updatedMember }, 
+                            { useFindAndModify: false, upsert: true, new: true },
+                            (err, result) => {
+                                console.log(result);
+                                res.send(result);
+                            })
+        .catch(err => console.log(err));
 })
 
+// Update a project
+app.patch('/updatePortfolio/:id', (req, res) => {
+  const _id = req.params.id;
+  const updatedProject = {
+                          title : req.body.title,
+                          description : req.body.description,
+                          image : req.body.image,
+                          category : req.body.category,
+                          price : req.body.price
+  };
+
+  Portfolio.findByIdAndUpdate(_id, 
+                                {$set: updatedProject}, 
+                                { upsert: true, new: true}, 
+                                (err, result) => {
+                                    res.send({
+                                      _id: result._id,
+                                      title: result.title,
+                                      description: result.description,
+                                      image: result.image,
+                                      category: result.category,
+                                      price: result.price,
+                                      memberId: result.memberId
+                                    })
+            })
+            .catch(err => res.send(err));
+})
+
+// Find and return a project
+app.get('/findProject/:id', (req, res) => {
+  let _projectID = req.params.id;
+
+  Portfolio.findById(_projectID, 
+                      (err, result) => { res.send(result); })
+})
+
+// Find and return a user's account information
+app.get('/myAccountInfo/:accountID', (req, res) => {
+  let _memberId = req.params.accountID;
+
+  Member.findById(_memberId, 
+                  (err, result) => { res.send(result); })
+})
+
+// Find and return all projects that belong to a user
 app.get('/myPortfolios/:accountID', (req, res) => {
   let _memberId = req.params.accountID;
-  Portfolio.find({ memberId: _memberId }, function(err, results) {
-    if(results.length > 0) {
-      res.send(results);
-    } else {
-      res.send('No portfolio by this user found');
-    }
+
+  Portfolio.find({ memberId: _memberId },
+                  (err, results) => {
+                      if(results.length > 0) {
+                        res.send(results);
+                      } else {
+                        res.send('No portfolio by this user found');
+                      }
   });
 });
 
+// Return all projects with corresponding author information
 app.get('/portfoliosAndAuthors', async (req, res) => {
   let query = await Portfolio.aggregate([
-    { $lookup: {
-                from: "members",
-                localField: "memberId",
-                foreignField: "_id",
-                as: "authorInfo"
-    }},
-    { $unwind: "$authorInfo" }
+                                        { $lookup: {
+                                                    from: "members",
+                                                    localField: "memberId",
+                                                    foreignField: "_id",
+                                                    as: "authorInfo"
+                                        }},
+                                        { $unwind: "$authorInfo" }
   ]);
   res.send(query);
 });
 
+// Return one project with corresponding author information
 app.get('/portfolioWithAuthor/:id', async (req, res) => {
   let artId = req.params.id;
   let query = await Portfolio.aggregate([
-    { $match: { _id: mongoose.Types.ObjectId(artId) }},
-    { $lookup: {
-                from: "members",
-                localField: "memberId",
-                foreignField: "_id",
-                as: "authorInfo"
-    }},
-    { $unwind: "$authorInfo" },
-    { $lookup: {
-                from: "comments",
-                localField: "_id",
-                foreignField: "portfolioID",
-                as: "comments"
-    }}
-  ])
-  console.log(query);
+                                        { $match: { _id: mongoose.Types.ObjectId(artId) }},
+                                        { $lookup: {
+                                                    from: "members",
+                                                    localField: "memberId",
+                                                    foreignField: "_id",
+                                                    as: "authorInfo"
+                                        }},
+                                        { $unwind: "$authorInfo" },
+                                        { $lookup: {
+                                                    from: "comments",
+                                                    localField: "_id",
+                                                    foreignField: "portfolioID",
+                                                    as: "comments"
+                                        }}
+  ]);
+
   res.send(query);
 });
 
+// Find and return all products that match with price and category filter
 app.get('/filterPortfolios/:minPrice/:maxPrice/:category', async (req, res) => {
   let _minPrice = parseInt(req.params.minPrice);
   let _maxPrice = parseInt(req.params.maxPrice);
@@ -200,25 +271,25 @@ app.get('/filterPortfolios/:minPrice/:maxPrice/:category', async (req, res) => {
 
   if(_category === "all") {
     query = await Portfolio.aggregate([
-      { $match: { price: { $gt: _minPrice, $lt: _maxPrice }}},
-      { $lookup: {
-                  from: "members",
-                  localField: "memberId",
-                  foreignField: "_id",
-                  as: "authorInfo"
-      }},
-      { $unwind: "$authorInfo" }
+                                      { $match: { price: { $gt: _minPrice, $lt: _maxPrice }}},
+                                      { $lookup: {
+                                                  from: "members",
+                                                  localField: "memberId",
+                                                  foreignField: "_id",
+                                                  as: "authorInfo"
+                                      }},
+                                      { $unwind: "$authorInfo" }
     ])
   } else {
     query = await Portfolio.aggregate([
-      { $match: { $and: [{ category: _category }, { price: { $gt: _minPrice, $lt: _maxPrice }}]}},
-      { $lookup: {
-                  from: "members",
-                  localField: "memberId",
-                  foreignField: "_id",
-                  as: "authorInfo"
-      }},
-      { $unwind: "$authorInfo" }
+                                      { $match: { $and: [{ category: _category }, { price: { $gt: _minPrice, $lt: _maxPrice }}]}},
+                                      { $lookup: {
+                                                  from: "members",
+                                                  localField: "memberId",
+                                                  foreignField: "_id",
+                                                  as: "authorInfo"
+                                      }},
+                                      { $unwind: "$authorInfo" }
     ])
   }
 
@@ -229,14 +300,15 @@ app.get('/filterPortfolios/:minPrice/:maxPrice/:category', async (req, res) => {
   }
 })
 
+// Add a comment to post
 app.post('/addComment', (req, res) => {
   let comment = new Comment({
-    _id : new mongoose.Types.ObjectId,
-    portfolioID: req.body.portfolioID,
-    postByID: mongoose.Types.ObjectId(req.body.postByID),
-    postByUsername: req.body.postByUsername,
-    posted: req.body.postDate,
-    text: req.body.content
+                            _id : new mongoose.Types.ObjectId,
+                            portfolioID: req.body.portfolioID,
+                            postByID: mongoose.Types.ObjectId(req.body.postByID),
+                            postByUsername: req.body.postByUsername,
+                            posted: req.body.postDate,
+                            text: req.body.content
   })
 
   comment.save()
@@ -245,6 +317,7 @@ app.post('/addComment', (req, res) => {
 })
 
 
-// Hayley's code ends
+// =========================== HAYLEY's code ends  ===================================
+// ===================================================================================
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
